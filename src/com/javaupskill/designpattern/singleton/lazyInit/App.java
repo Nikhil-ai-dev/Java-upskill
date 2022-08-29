c502a46f6c59fff66a6ff36f48fe44a3a752850c
Nikhil-ai-dev <nikhil.nn245@gmail.com>
2022-08-29 23:17:32 +0530

    Initial Commit

---
+++ src/com/javaupskill/designpattern/singleton/lazyInit/App.java
@@ -0,0 +1,13 @@
+package com.javaupskill.designpattern.singleton.lazyInit;
+
+public class App {
+    public  static void main (String args[]){
+        //Calling first time to create new instance
+        SingletonLazyInit newObject = SingletonLazyInit.getSingletonLazyInitObject();
+        //will return existing Instance
+        SingletonLazyInit existingObject = SingletonLazyInit.getSingletonLazyInitObject();
+        if(newObject.equals(existingObject)){
+            System.out.println("Object 1 and 2 are equal");
+        }
+    }
+}
