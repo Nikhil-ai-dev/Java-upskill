293e270c4928971b44b9ec2a92cf64970956383a
Nikhil-ai-dev <nikhil.nn245@gmail.com>
2022-08-31 17:30:00 +0530

    #comment testing git commit from intj

--- src/com/javaupskill/designpattern/singleton/lazyInit/App.java
+++ src/com/javaupskill/designpattern/singleton/lazyInit/App.java
@@ -4,7 +4,7 @@ public class App {
     public  static void main (String args[]){
         //Calling first time to create new instance
         SingletonLazyInit newObject = SingletonLazyInit.getSingletonLazyInitObject();
-        //will return existing Instance
+        //will return existing Instance instead of creating a new instance
         SingletonLazyInit existingObject = SingletonLazyInit.getSingletonLazyInitObject();
         if(newObject.equals(existingObject)){
             System.out.println("Object 1 and 2 are equal");
